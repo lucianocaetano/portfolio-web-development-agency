ced9ef82813baf8b0a0eae58536fc5235ce32f7e
elsantiF <santiagofarall@gmail.com>
2025-05-15 16:20:30 -0300

    feat: new footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,12 @@
-import Image from 'next/image'
+import { MailIcon, LinkedinIcon } from 'lucide-react'
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col h-[250px] w-full items-center justify-center border-t bg-foreground text-background">
-      <Image src="/footer.svg" width={40} height={40} alt="logo" />
-      <h2 className="text-2xl font-semibold">BACK TO TOP</h2>
-      <p>
-        <span className="font-semibold">@2020 Percy pockets solution</span> All Rights Reserved.
-      </p>
+    <footer className="flex flex-col h-32 w-full items-center justify-center bg-foreground text-background">
+      <p>Percy Pocket Solutions &copy; 2025</p>
       <div className="flex gap-6 mt-6">
-        <Image src="/email.svg" alt="" width={30} height={30} className="cursor-pointer" />
-        <Image src="/linkedin.svg" alt="" width={30} height={30} className="cursor-pointer" />
+        <MailIcon size={24} strokeWidth={1.5} className="cursor-pointer" />
+        <LinkedinIcon size={24} strokeWidth={1.5} className="cursor-pointer" />
       </div>
     </footer>
   )

6fd879b3b93cad031438a1e2d19db5b56e4a8d48
elsantiF <santiagofarall@gmail.com>
2025-05-16 19:14:37 -0300

    fix: remove ContactSection from 'about'

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -2,7 +2,6 @@ import Banner from "./components/banner"
 import WhoWeAre from "./components/who-we-are"
 import SolutionsSection from "./components/solutions_section"
 import OurMethodology from "./components/our-methodology"
-import ContactSection from "./components/contact-section"
 
 const About = () => {
 

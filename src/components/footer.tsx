ee9d8affebd772e7df5fc07d17c51ea6687b9c50
LucianoMosca <lmd62018@gmail.com>
2025-05-16 21:29:45 -0300

    fix linkedin opens in new tab instead of same one

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <footer className="flex flex-col h-32 w-full items-center justify-center bg-foreground text-background">
       <p>Percy Pocket Solutions &copy; 2025</p>
       <div className="flex gap-6 mt-6">
-        <a href="mailto:contactus@percypocket.solutions" aria-label="Email">
+        <a href="mailto:contactus@percypocket.solutions" aria-label="Email" >
            <MailIcon size={24} strokeWidth={1.5} className="hover:opacity-80 transition-opacity" />
         </a>
-        <a href="https://www.linkedin.com/company/percy-pocket-solutions/" aria-label="Linkedin">
+        <a href="https://www.linkedin.com/company/percy-pocket-solutions/" aria-label="Linkedin" target="_blank" rel="noopener noreferrer">
            <LinkedinIcon size={24} strokeWidth={1.5} className="cursor-pointer" />
         </a>
       </div>

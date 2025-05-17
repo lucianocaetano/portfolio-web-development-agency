de19fe1a4d51a8a83ce9458ec22f9d270037530d
LucianoMosca <lmd62018@gmail.com>
2025-05-17 18:26:16 -0300

    Se hace explicito el pointer-events-auto por si netlify lo está purgando por defecto al estar implicito

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <footer className="flex flex-col h-32 w-full items-center justify-center bg-foreground text-background">
       <p>Percy Pocket Solutions &copy; 2025</p>
       <div className="flex gap-6 mt-6">
-        <a href="mailto:contactus@percypocket.solutions" aria-label="Email" >
-           <MailIcon size={24} strokeWidth={1.5} className="hover:opacity-80 transition-opacity" />
+        <a href="mailto:contactus@percypocket.solutions" aria-label="Email" className='"cursor-pointer pointer-events-auto"' >
+           <MailIcon size={24} strokeWidth={1.5} />
         </a>
-        <a href="https://www.linkedin.com/company/percy-pocket-solutions/" aria-label="Linkedin" target="_blank" rel="noopener noreferrer">
-           <LinkedinIcon size={24} strokeWidth={1.5} className="cursor-pointer" />
+        <a href="https://www.linkedin.com/company/percy-pocket-solutions/" aria-label="Linkedin" target="_blank" rel="noopener noreferrer" className='"cursor-pointer pointer-events-auto"'>
+           <LinkedinIcon size={24} strokeWidth={1.5} />
         </a>
       </div>
     </footer>

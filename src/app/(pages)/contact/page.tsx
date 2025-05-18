c5c65d841575bcdeee60d53165e0c5c83f34b68a
elsantiF <santiagofarall@gmail.com>
2025-05-18 15:44:00 -0300

    feat: new contact design

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -3,16 +3,16 @@ import { ContactForm } from "./components/contact-form";
 const ContactPage: React.FC = () => {
   return (
     <>
-      <div className="flex max-xl:flex-col justify-between px-8 w-full pt-16">
-        <div className="w-fit self-center md:self-start">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-start">
+      <div className="flex flex-col md:justify-around md:my-8 w-full items-center">
+        <div className="flex flex-col items-center w-fit">
+          <h1 className="scroll-m-20 text-5xl font-bold tracking-tight text-start">
             Contact Us
           </h1>
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             Feel free to reach out to us!
           </p>
         </div>
-        <div className="mt-8 w-fit self-center md:self-start">
+        <div className="mt-8 w-fit">
           <ContactForm />
         </div>
       </div>

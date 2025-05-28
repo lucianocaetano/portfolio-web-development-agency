dcfc4eebf0a5c34e61c7c96812d6e8f7da08caa8
elsantiF <santiagofarall@gmail.com>
2025-05-28 18:21:19 -0300

    feat: added @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), playformCompress()],
 
   vite: {
     plugins: [tailwindcss()],
